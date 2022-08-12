fb3c532c331653925b73a6888ad38af7cbbf2d35
Aalam <aalamansari1712@gmail.com>
2022-08-12 10:31:32 +0530

    added code in Get_label method

--- test.py
+++ test.py
@@ -20,10 +20,15 @@ def Init_model():
     
 
 def Get_labels():
-    with open('labels.txt','r'):
+    labels=list()
+    with open('Office_model/labels.txt','r') as file:
+        data=file.readlines()
+        for line in data:
+            labels.append(line[2:].rstrip())
+    return labels
         
 
-def Det_cam(model,data):
+def Det_cam(model,data,labels):
     vid = cv2.VideoCapture(0)
     count=0
 
@@ -44,15 +49,12 @@ def Det_cam(model,data):
         prediction = model.predict(data)
         # print(prediction)
 
-        for i in prediction:
-            if i[0]>0.7:
-                text="person"
-            if i[1]>0.7:
-                text="car"
-            if i[2]>0.7:
-                text="plane"    
-            image=cv2.resize(image,(500,500))
-            cv2.putText(image,text,(10,30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,255),2,cv2.LINE_8)
+        val=prediction[0]
+        index=(np.where(val==np.amax(val)))[0][0]
+        text=labels[index]
+               
+        image=cv2.resize(image,(500,500))
+        cv2.putText(image,text,(10,30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,255),2,cv2.LINE_8)
         cv2.imshow("test",image)
 
 
